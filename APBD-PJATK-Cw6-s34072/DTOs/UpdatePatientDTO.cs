c9527159b6b275059069894e5b44417f0bf3180d
Krzysztof Lisowski <s34072@pjwstk.edu.pl>
2026-04-28 21:46:15 +0200

    Dodanie UpdatePatientDTO do obslugi zadan PUT

---
+++ APBD-PJATK-Cw6-s34072/DTOs/UpdatePatientDTO.cs
@@ -0,0 +1,10 @@
+namespace APBD_PJATK_Cw6_s34072.DTOs;
+
+public class UpdatePatientDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
